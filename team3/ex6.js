f1a933e05a698943b58ec6e188f035b58e72b7cc
Ionescu Valentin Stefan <94042909+StefanIv21@users.noreply.github.com>
2023-02-18 13:45:33 +0200

    Delete team3 directory

--- team3/ex6.js
+++
@@ -1,6 +0,0 @@
-const arr=[12,2,4,5,7,8,6];
-var sum=0;
-arr.forEach(element =>{
-    sum +=element
-});
-console.log(sum);
--- team3/ex7p2.js
+++
@@ -1,4 +0,0 @@
-import func from './ex7.js'
-func();
-import { arr} from './ex7.js'
-console.log(arr);
