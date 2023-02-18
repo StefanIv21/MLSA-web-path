ca8dae392b1b0d40adf2fa33b7b23494bc34cccf
Ionescu Valentin Stefan <94042909+StefanIv21@users.noreply.github.com>
2023-02-18 13:44:54 +0200

    Add files via upload

---
+++ team3/ex6.js
@@ -0,0 +1,6 @@
+const arr=[12,2,4,5,7,8,6];
+var sum=0;
+arr.forEach(element =>{
+    sum +=element
+});
+console.log(sum);
---
+++ team3/ex7p2.js
@@ -0,0 +1,4 @@
+import func from './ex7.js'
+func();
+import { arr} from './ex7.js'
+console.log(arr);
